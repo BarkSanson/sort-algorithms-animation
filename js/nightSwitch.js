235b53ddf4f056cad8b1ec6d41b45d328e1f18f3
Bark Sanson <barkSansonDev@gmail.com>
2021-08-01 18:40:05 +0200

    Sidebar now has lightmode

--- js/nightSwitch.js
+++ js/nightSwitch.js
@@ -1,14 +1,23 @@
 export function changeStyle() {
-    const nav = document.querySelector("nav");
-    const navBtns = nav.querySelectorAll("button");
-    const inputBtns = document.querySelectorAll(".input-btn");
-    inputBtns.forEach((btn) => {
+    var _a, _b, _c;
+    var nav = document.querySelector("nav");
+    var sidebar = document.querySelector("#sidebar");
+    var sidebarHeader = document.getElementById("sidebar-header");
+    var navBtns = nav.querySelectorAll("button");
+    var inputBtns = document.querySelectorAll(".input-btn");
+    var algorithmSelector = document.getElementById("algorithm-selector");
+    inputBtns.forEach(function (btn) {
         btn.classList.toggle("light-input-btn");
     });
     document.querySelector("body").classList.toggle("light-body");
+    sidebar.classList.toggle("light-sidebar");
+    sidebarHeader === null || sidebarHeader === void 0 ? void 0 : sidebarHeader.classList.toggle("light-sidebar-header");
+    (_a = sidebarHeader.firstElementChild.firstElementChild) === null || _a === void 0 ? void 0 : _a.classList.toggle("light-sidebar-header-contents");
+    (_b = sidebarHeader === null || sidebarHeader === void 0 ? void 0 : sidebarHeader.lastElementChild) === null || _b === void 0 ? void 0 : _b.classList.toggle("light-sidebar-header-contents");
+    (_c = algorithmSelector === null || algorithmSelector === void 0 ? void 0 : algorithmSelector.firstElementChild) === null || _c === void 0 ? void 0 : _c.classList.toggle("light-sidebar-header-contents");
     nav.classList.toggle("light-nav");
     nav.children[1].classList.toggle("light-nav-contents");
-    navBtns.forEach((btn) => {
+    navBtns.forEach(function (btn) {
         btn.children[0].classList.toggle("light-nav-contents");
     });
 }
--- ts/nightSwitch.ts
+++ ts/nightSwitch.ts
@@ -1,13 +1,24 @@
 export function changeStyle() {
     const nav = document.querySelector("nav");
+    const sidebar = document.querySelector("#sidebar");
+    const sidebarHeader = document.getElementById("sidebar-header");
     const navBtns = nav!.querySelectorAll("button");
     const inputBtns = document.querySelectorAll(".input-btn");
+    const algorithmSelector = document.getElementById("algorithm-selector");
 
     inputBtns.forEach((btn) => {
         btn.classList.toggle("light-input-btn");
     });
     
-    document.querySelector("body")!.classList.toggle("light-body");   
+    document.querySelector("body")!.classList.toggle("light-body");
+    sidebar!.classList.toggle("light-sidebar");
+
+    sidebarHeader?.classList.toggle("light-sidebar-header");
+    sidebarHeader!.firstElementChild!.firstElementChild?.classList.toggle("light-sidebar-header-contents");
+    sidebarHeader?.lastElementChild?.classList.toggle("light-sidebar-header-contents");
+
+    algorithmSelector?.firstElementChild?.classList.toggle("light-sidebar-header-contents");
+
     nav!.classList.toggle("light-nav"); 
     nav!.children[1].classList.toggle("light-nav-contents");
 
